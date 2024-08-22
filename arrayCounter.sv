1dc1adc8486980c08320c77ff173db950c865841
Krishanth Suthaharan <krishanth2001@live.com>
2024-08-22 16:06:11 -0400

    Instead of using a function, came up with a more simpler strategy to count ones using always and for loop blocks of code

--- arrayCounter.sv
+++ arrayCounter.sv
@@ -16,3 +16,18 @@ module array_counter(
     assign out = count_ones(in);
 
 endmodule
+
+module array_counter_2.0( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    always @(*) begin	// Combinational always block
+		out = 0;
+        for (int i=0;i<255;i++) begin
+            if(in[i]) begin
+            	out = out + 1;
+            end
+        end
+	  end
+
+endmodule
