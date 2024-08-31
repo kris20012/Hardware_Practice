a1f4846541dbb2c58f677e87623441b5ff9b3d6b
Krishanth Suthaharan <krishanth2001@live.com>
2024-08-31 15:56:31 -0400

    Implemented module to shift bits right

---
+++ shift_register.sv
@@ -0,0 +1,18 @@
+module shift_register_4_bit(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q); 
+    
+    always@(areset, clk) begin
+        if(areset)
+            q <= 4'b0;
+        else if(load)
+            q <= data;
+        else if(ena)
+            q <= q >> 1;
+    end
+
+endmodule
